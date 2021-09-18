39446b56ac988434c0ee20950d0cf546ac45c3e0
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-09-18 10:53:11 +0530

    Added Bershelf Dependency Management and Data bag lab exercises.

--- Day4/chef-repo/cookbooks/demo/metadata.rb
+++ Day4/chef-repo/cookbooks/demo/metadata.rb
@@ -3,9 +3,9 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures demo'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 16.0'
-
+depends 'apt', '~> 5.0'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
